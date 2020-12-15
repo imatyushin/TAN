a01803840f30cb89b193ba26a225ceddf77cc6e5
ivm <ivm>
2020-12-15 19:02:19 +0300

    + corrected generators

--- tan/build/cmake/generate-xcode-cl.sh
+++ tan/build/cmake/generate-xcode-cl.sh
@@ -5,6 +5,6 @@ rm -rf ./mac-xcode-cl
 mkdir mac-xcode-cl
 cd mac-xcode-cl
 
-cmake -G "Xcode" .. -DCMAKE_PREFIX_PATH="/Applications/Qt5.6.3/5.6.3/clang_64" -DOpenCL_INCLUDE_DIR="../../../../thirdparty/OpenCL-Headers" -DPortAudio_DIR="../../../../../thirdparty/portaudio" -DDEFINE_AMD_OPENCL_EXTENSION=1 -DAMF_OPEN_DIR="../../../amfOpen" -DTAN_NO_OPENCL=0
+cmake -G "Xcode" .. -DCMAKE_PREFIX_PATH="/Applications/Qt5.6.3/5.6.3/clang_64" -DOpenCL_INCLUDE_DIR="../../../../thirdparty/OpenCL-Headers" -DPortAudio_DIR="../../../../../thirdparty/portaudio" -DDEFINE_AMD_OPENCL_EXTENSION=1 -DAMF_OPEN_DIR="../../../amfOpen" -DTAN_NO_OPENCL=0 -DAMF_CORE_STATIC=1
 
 cd ..
