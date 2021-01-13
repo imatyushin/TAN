4ef5f296d7f44d2fd2a17bfbaf86c2ce5b9318f8
ivm <ivm>
2021-01-13 14:00:42 +0300

    build system updated to build fftw against TAN for Mac and others

--- tan/build/cmake/setup_OpenCL.cmake
+++ tan/build/cmake/setup_OpenCL.cmake
@@ -9,6 +9,26 @@ set(CMAKE_SKIP_RULE_DEPENDENCY TRUE)
 get_property(OpenCL_FOUND GLOBAL PROPERTY OpenCL_FOUND)
 
 if(NOT OpenCL_FOUND)
+  message("")
+  message("OpenCL_INCLUDE_DIR is set to " ${OpenCL_INCLUDE_DIR})
+
+  #test the ${OpenCL_INCLUDE_DIR} is not an absolute path
+  if(NOT IS_ABSOLUTE ${OpenCL_INCLUDE_DIR})
+    #test this cmake is included from other project
+    if(NOT ${CMAKE_CURRENT_SOURCE_DIR} STREQUAL ${CMAKE_SOURCE_DIR})
+      get_filename_component(Path_To_OpenCL "${CMAKE_SOURCE_DIR}/${OpenCL_INCLUDE_DIR}" ABSOLUTE)
+    #assume that the project is built from itself
+    else()
+      get_filename_component(Path_To_OpenCL "${CMAKE_CURRENT_SOURCE_DIR}/${OpenCL_INCLUDE_DIR}" ABSOLUTE)
+    endif()
+  else()
+    get_filename_component(Path_To_OpenCL ${OpenCL_INCLUDE_DIR} ABSOLUTE)
+  endif()
+
+  message("OpenCL_INCLUDE_DIR expanded: " ${Path_To_OpenCL})
+  message("search OpenCL...")
+
+  #search
   find_package(OpenCL REQUIRED)
 
   if(NOT OpenCL_FOUND)
@@ -17,17 +37,17 @@ if(NOT OpenCL_FOUND)
     return()
   else()
     message("OpenCL found: " ${OpenCL_FOUND})
-    message("OpenCL_INCLUDE_DIRS: " ${OpenCL_INCLUDE_DIRS})
+    message("OpenCL_INCLUDE_DIRS: " ${Path_To_OpenCL})
     message("OpenCL_LIBRARIES: " ${OpenCL_LIBRARIES})
     message("OpenCL_VERSION_STRING: " ${OpenCL_VERSION_STRING})
     message("OpenCL_VERSION_MAJOR: " ${OpenCL_VERSION_MAJOR})
     message("OpenCL_VERSION_MINOR: " ${OpenCL_VERSION_MINOR})
 
-    message("OpenCL_INCLUDE_DIR: ${OpenCL_INCLUDE_DIR}")
+    message("OpenCL_INCLUDE_DIR: ${Path_To_OpenCL}")
     message("OpenCL_LIBRARY: ${OpenCL_LIBRARY}")
 
     set_property(GLOBAL PROPERTY OpenCL_FOUND 1)
-    set_property(GLOBAL PROPERTY OpenCL_INCLUDE_DIR ${OpenCL_INCLUDE_DIR})
+    set_property(GLOBAL PROPERTY OpenCL_INCLUDE_DIR ${Path_To_OpenCL})
     set_property(GLOBAL PROPERTY OpenCL_LIBRARY ${OpenCL_LIBRARY})
   endif()
 endif()
@@ -43,6 +63,7 @@ get_property(OpenCL_LIBRARY GLOBAL PROPERTY OpenCL_LIBRARY)
 
 include_directories(${OpenCL_INCLUDE_DIR})
 link_directories(${OpenCL_LIBRARY})
+
 ADD_DEFINITIONS(-DCL_TARGET_OPENCL_VERSION=120)
 
 if(DEFINE_AMD_OPENCL_EXTENSION)
