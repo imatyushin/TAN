918ab65737ccc99158147b1b65adf5c514b3cbee
iterator@inbox.ru <iterator@inbox.ru>
2021-03-29 17:31:15 +0300

    hotfix

--- tan/build/cmake/generate-vs2017-cl.bat
+++ tan/build/cmake/generate-vs2017-cl.bat
@@ -6,4 +6,4 @@ MKDIR vs2017-cl
 CD vs2017-cl
 
 SET CMAKE_PREFIX_PATH=c:\Qt\Qt5.6.3\5.6.3\msvc2015_64
-cmake .. -G "Visual Studio 15 2017" -A x64 -DOpenCL_INCLUDE_DIR="../../../thirdparty/OpenCL-Headers" -DOpenCL_LIBRARY="C:\Program Files (x86)\OCL_SDK_Light\lib\x86_64\OpenCL.lib" -DPortAudio_DIR="../../../thirdparty/portaudio" -DFFTW_DIR="../../../thirdparty/fftw" -DAMF_OPEN_DIR="../../../amfOpen" -DDEFINE_AMD_OPENCL_EXTENSION=1
+cmake .. -G "Visual Studio 15 2017" -A x64 -DOpenCL_INCLUDE_DIR="../../../thirdparty/OpenCL-Headers" -DOpenCL_LIBRARY="C:\Program Files (x86)\OCL_SDK_Light\lib\x86_64\OpenCL.lib" -DPortAudio_DIR="../../../thirdparty/portaudio" -DFFTW_DIR="../../../thirdparty/fftw" -DAMF_OPEN_DIR="../../../amfOpen" -DAMF_CORE_SHARED=1 -DDEFINE_AMD_OPENCL_EXTENSION=1
